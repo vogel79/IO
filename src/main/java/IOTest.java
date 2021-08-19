2128d890eb57e0ba49d99885209cc5d3e6a03638
Evgeniya Lyahovec <jacky@ukr.net>
2021-08-19 23:02:24 +0300

    IO Project

---
+++ src/main/java/IOTest.java
@@ -0,0 +1,7 @@
+public class IOTest {
+    public static void main(String[] args) {
+        AppData appData = new AppData();
+        RWCSVFile.saveDataToFile(appData);
+        RWCSVFile.readDataFromFile(RWCSVFile.CSVFile);
+    }
+}
